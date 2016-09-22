09f174cab490ae437a36a256a5eda49d4ecf62bc
FGalerne <florian.galerne@free.fr>
2016-09-22 10:07:10 +0200

    Quête phpStorm

---
+++ phpstormquete.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: florian
+ * Date: 20/09/16
+ * Time: 23:06
+ */
+
+$students = [
+    "Emmanuel"  => 42,
+    "Thierry"   => 51,
+    "Pascal"    => 45,
+    "Eric"      => 48,
+    "Nicolas"   => 19
+];
+foreach ($students as $student){
+    $moy = $student/5;
+
+
+};
+echo "la moyenne des éléves de : ". $moy;
+
